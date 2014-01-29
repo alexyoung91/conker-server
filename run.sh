0681050295995fbfdc57eae15b8e28e871f3644f
alexyoung91 <alexyoung91@googlemail.com>
2014-01-29 02:18:34 +0000

    updated run dependencies

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
-    java -cp "bin:lib/servlet-api-2.5.jar:lib/jetty-all-9.0.4.v20130625.jar:lib/mysql-connector-java-5.1.28-bin.jar" co.conker.Server
+    java -cp ".:bin/:lib/javax.servlet-3.0.0.v201112011016.jar:lib/jetty-all-9.0.4.v20130625.jar:lib/mysql-connector-java-5.1.28-bin.jar" co.conker.ConkerServer
 else
-	java -cp "bin;lib/servlet-api-2.5.jar;lib/jetty-all-9.0.4.v20130625.jar;lib/mysql-connector-java-5.1.28-bin.jar" co.conker.Server
+	java -cp ".;bin/;lib/javax.servlet-3.0.0.v201112011016.jar;lib/jetty-all-9.0.4.v20130625.jar;lib/mysql-connector-java-5.1.28-bin.jar" co.conker.ConkerServer
 fi
