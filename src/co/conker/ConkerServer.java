a126e0b727c6cb0b71d6862271da5944b6d6e67b
alexyoung91 <alexyoung91@googlemail.com>
2014-01-29 03:34:01 +0000

    Import only JSONObject

--- src/co/conker/ConkerServer.java
+++ src/co/conker/ConkerServer.java
@@ -16,7 +16,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 
-import org.json.*;
+import org.json.JSONObject;
 
 public class ConkerServer extends AbstractHandler {
 
