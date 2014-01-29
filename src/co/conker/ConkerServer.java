0a7b846601ae792d06d774d906c797948c3b4a71
alexyoung91 <alexyoung91@googlemail.com>
2014-01-29 02:35:56 +0000

    added db connectivity

--- src/co/conker/ConkerServer.java
+++ src/co/conker/ConkerServer.java
@@ -18,36 +18,56 @@ import java.sql.Statement;
 
 public class ConkerServer extends AbstractHandler {
 
+	private Connection conn;
+	private static String dbURL;
+	private static String dbUsername;
+	private static String dbPassword;
+
+	public ConkerServer() {
+		// Connect to MySQL
+		conn = null;
+		dbURL = "jdbc:mysql://localhost:3306/Conker";
+		dbUsername = "conker";
+		dbPassword = "lolbanter2910";
+		
+		try {
+			conn = DriverManager.getConnection(dbURL, dbUsername, dbPassword);
+		} catch (Exception e) {
+			System.out.println("Failed to connect to MySQL! Error: " + e);
+			System.exit(0);
+		}
+	}
+
 	@Override
 	public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         response.setContentType("text/html;charset=utf-8");
         response.setStatus(HttpServletResponse.SC_OK);
         baseRequest.setHandled(true);
-        response.getWriter().println("<h1>Hello World</h1>");
+		
+		Statement stmt = dbCon.createStatement();
+        ResultSet res = stmt.executeQuery("SELECT * FROM User");
+		
+		String names = "";
+		
+		while(buildingsRS.next()) {
+			//Integer id = buildingsRS.getInt("id");
+			String firstName = res.getString("firstName");
+			names += firstName + " ";
+			//String hexcode = buildingsRS.getString("hexcode");
+			//String description = buildingsRS.getString("description");
+			//String centre_latitude = buildingsRS.getString("centre_latitude");
+			//String centre_longitude = buildingsRS.getString("centre_longitude");
+			//System.out.println("Building [Name: " + name + " | Hexcode: " + hexcode + " | Description: " + description + "]");
+		}
+		
+        //response.getWriter().println("<h1>Hello World</h1>");
+		response.getWriter().println(names);
     }
 	
 	public static void main(String[] args) throws Exception {
-		System.out.println("DB Test");
-		
 		Server server = new Server(8080);
         server.setHandler(new ConkerServer());
-		
-		// Connect to MySQL
-		//Connection dbCon = null;
-		//String dbURL = "jdbc:mysql://localhost:3306/Conker";
-		//String dbUsername = "conker";
-		//String dbPassword = "lolbanter2910";
-
-		/*
-		try {
-			dbCon = DriverManager.getConnection(dbURL, dbUsername, dbPassword);
-		} catch (Exception e) {
-			System.out.println("Failed to connect to MySQL! Error: " + e);
-		}
-		*/
-		
-		System.out.println("Connected OK.");
- 
+		 
         server.start();
         server.join();
 	}
