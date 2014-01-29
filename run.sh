111123e2e628245dc870900f3eb838907fde98b6
alexyoung91 <alexyoung91@googlemail.com>
2014-01-29 02:13:14 +0000

    Updated libraries, and build/run files

--- run.sh
+++ run.sh
@@ -1,1 +1,7 @@
-java -cp "bin/:lib/*" co.conker.Server
+java -cp "bin:lib/servlet-api-2.5.jar:lib/jetty-all-9.0.4.v20130625.jar:lib/mysql-connector-java-5.1.28-bin.jar" co.conker.Server
+
+if [ "$(expr substr $(uname -s) 1 5)" == "Linux" ]; then
+    java -cp "bin:lib/servlet-api-2.5.jar:lib/jetty-all-9.0.4.v20130625.jar:lib/mysql-connector-java-5.1.28-bin.jar" co.conker.Server
+else
+	java -cp "bin;lib/servlet-api-2.5.jar;lib/jetty-all-9.0.4.v20130625.jar;lib/mysql-connector-java-5.1.28-bin.jar" co.conker.Server
+fi
