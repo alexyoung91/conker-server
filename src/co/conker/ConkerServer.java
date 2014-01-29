06110c76ac7900b27cf8a392046bbd9f3c2e2c39
alexyoung91 <alexyoung91@googlemail.com>
2014-01-29 02:14:14 +0000

    removed duplicate method definition

--- src/co/conker/ConkerServer.java
+++ src/co/conker/ConkerServer.java
@@ -26,14 +26,6 @@ public class ConkerServer extends AbstractHandler {
         response.getWriter().println("<h1>Hello World</h1>");
     }
 	
-	@Override
-    public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        response.setContentType("text/html;charset=utf-8");
-        response.setStatus(HttpServletResponse.SC_OK);
-        baseRequest.setHandled(true);
-        response.getWriter().println("<h1>Hello World</h1>");
-    }
-	
 	public static void main(String[] args) throws Exception {
 		System.out.println("DB Test");
 		
