f7ed8860d455efe77e1ce90b79f3b1510bda402a
Alex Young <alexyoung91@googlemail.com>
2014-01-30 00:12:25 +0000

    Temporary path handling implemented

--- src/co/conker/ConkerServer.java
+++ src/co/conker/ConkerServer.java
@@ -20,6 +20,8 @@ import org.json.JSONObject;
 import org.json.JSONArray;
 import org.json.JSONException;
 
+import java.util.Date;
+
 public class ConkerServer extends AbstractHandler {
 
 	private Connection conn;
@@ -37,68 +39,90 @@ public class ConkerServer extends AbstractHandler {
 
 	@Override
 	public void handle(String target, Request baseRequest, HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
-        response.setContentType("application/json;charset=utf-8");
-        response.setStatus(HttpServletResponse.SC_OK);
-        baseRequest.setHandled(true);
+	
+		//log("-------------------------------");
+		//log("URL: " + request.getRequestURL());
+		//log("URI: " + request.getRequestURI());
 		
-		String output = "-";
-		
-		JSONArray projectsArray;
-		
-		try {
-			projectsArray = new JSONArray();
-			
-			try {
-				conn = DriverManager.getConnection(dbURL, dbUsername, dbPassword);
-			} catch (Exception e) {
-				System.out.println("Failed to connect to MySQL! Error: " + e);
-				System.exit(0);
-			}
-			
-			try {
-				// Join with user table??
-				Statement stmt = conn.createStatement();
-				ResultSet res = stmt.executeQuery("SELECT * FROM User, Project, ProjectPhoto WHERE User.id = Project.userID AND ProjectPhoto.projectID = Project.id ORDER BY Project.id ASC;");
+		switch (request.getRequestURI()) {
+			case "/projects": {
+				String latitude = request.getParameter("lat");
+				String longitude = request.getParameter("long");
 				
-				while(res.next()) {
-					JSONObject project = new JSONObject();
-					project.put("title", res.getString("title"));
-					project.put("description", res.getString("description"));
-					project.put("noVolunteersNeeded", res.getInt("noVolunteersNeeded"));
-					project.put("locationLat", res.getDouble("locationLat"));
-					project.put("locationLong", res.getDouble("locationLong"));
-					project.put("startDay", res.getInt("startDay"));
-					project.put("startMonth", res.getInt("startMonth"));
-					project.put("startYear", res.getInt("startYear"));
-					project.put("endDay", res.getInt("endDay"));
-					project.put("endMonth", res.getInt("endMonth"));
-					project.put("endYear", res.getInt("endYear"));
-					project.put("authorFirstName", res.getString("firstName"));
-					project.put("authorLastName", res.getString("User.lastName"));
-					project.put("photoSource", res.getString("source"));
-					projectsArray.put(project);
+				response.setContentType("application/json;charset=utf-8");
+				response.setStatus(HttpServletResponse.SC_OK);
+				baseRequest.setHandled(true);
+				
+				JSONArray projectsArray;
+				
+				try {
+					projectsArray = new JSONArray();
+					
+					try {
+						Class.forName("com.mysql.jdbc.Driver");
+						conn = DriverManager.getConnection(dbURL, dbUsername, dbPassword);
+					} catch (Exception e) {
+						System.out.println("Failed to connect to MySQL! Error: " + e);
+						System.exit(0);
+					}
+					
+					try {
+						// Join with user table??
+						Statement stmt = conn.createStatement();
+						ResultSet res = stmt.executeQuery("SELECT * FROM User, Project, ProjectPhoto WHERE User.id = Project.userID AND ProjectPhoto.projectID = Project.id ORDER BY Project.id ASC;");
+						
+						while(res.next()) {
+							JSONObject project = new JSONObject();
+							project.put("title", res.getString("title"));
+							project.put("description", res.getString("description"));
+							project.put("noVolunteersNeeded", res.getInt("noVolunteersNeeded"));
+							project.put("locationLat", res.getDouble("locationLat"));
+							project.put("locationLong", res.getDouble("locationLong"));
+							project.put("startDay", res.getInt("startDay"));
+							project.put("startMonth", res.getInt("startMonth"));
+							project.put("startYear", res.getInt("startYear"));
+							project.put("endDay", res.getInt("endDay"));
+							project.put("endMonth", res.getInt("endMonth"));
+							project.put("endYear", res.getInt("endYear"));
+							project.put("authorFirstName", res.getString("firstName"));
+							project.put("authorLastName", res.getString("User.lastName"));
+							project.put("photoSource", res.getString("source"));
+							projectsArray.put(project);
+						}
+						
+						stmt = null;
+						res = null;
+						
+						JSONObject o = new JSONObject();
+						o.put("projects", projectsArray);
+						
+						response.getWriter().println(o.toString(4));
+						
+					} catch (Exception e) {
+						log("Failed to execute SQL query! Error: " + e);
+						System.exit(0);
+					}
+				} catch (JSONException e) {
+					log("JSON Error.");
+					System.exit(0);
 				}
 				
-				stmt = null;
-				res = null;
+				conn = null;
 				
-				JSONObject o = new JSONObject();
-				o.put("projects", projectsArray);
-				
-				output = o.toString(4);
-				
-			} catch (Exception e) {
-				System.out.println("Failed to execute SQL query! Error: " + e);
-				System.exit(0);
+				log(latitude);
+				break;
 			}
-		} catch (JSONException e) {
-			System.out.println("JSON Error.");
-			System.exit(0);
+			case "/projectImage": {
+			
+				response.setContentType("image/png");
+				response.setStatus(HttpServletResponse.SC_OK);
+				baseRequest.setHandled(true);
+				
+				
+				
+				break;
+			}
 		}
-		
-		conn = null;
-		
-		response.getWriter().println(output);
     }
 	
 	public static void main(String[] args) throws Exception {
@@ -107,6 +131,10 @@ public class ConkerServer extends AbstractHandler {
 		 
         server.start();
         server.join();
+	}
+	
+	public static void log(String msg) {
+		System.out.println((new Date()).toString() + ": " + msg);
 	}
 }
 
