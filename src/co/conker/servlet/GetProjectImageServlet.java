86935e31512f25710094c1338aa9c75daa952bc3
Alex Young <alexyoung91@googlemail.com>
2014-02-06 15:53:55 +0000

    Removed a couple of comments

--- src/co/conker/servlet/GetProjectImageServlet.java
+++ src/co/conker/servlet/GetProjectImageServlet.java
@@ -114,7 +114,6 @@ public class GetProjectImageServlet extends HttpServlet {
                 output.write(buffer, 0, length);
             }
         } finally {
-            // Gently close streams.
             output.close();
             input.close();
         }
--- src/co/conker/servlet/GetUserImageServlet.java
+++ src/co/conker/servlet/GetUserImageServlet.java
@@ -114,7 +114,6 @@ public class GetUserImageServlet extends HttpServlet {
                 output.write(buffer, 0, length);
             }
         } finally {
-            // Gently close streams.
             output.close();
             input.close();
         }
